de221e06fc4855cb532bd030168d41611bef8bc5
QiangLaptop <qli@techfak.uni-bielefeld.de>
2016-03-03 22:43:17 +0100

    normal direction estimation(use tactool contact one point) is successful. more evaluation is needed, explain why the tool tangent surface is not in the same plane.

--- est_nv_tac.m
+++ est_nv_tac.m
@@ -38,6 +38,10 @@ for j =1:1:sample_num
     %estimate the linear velocity of tool, which is also the linear
     %velocity of robot end-effector
     tool_lv_dot_global = new_tool_end_eff_frame(1:3,4) - T_tool_end_eff_cur(1:3,4);
+    %draw tool: bar+square
+    myrmexsize = 0.08;
+    color = [0.3,0.6,0.8];
+    drawtactool(T_robot_end_eff_cur,T_tool_end_eff_cur,myrmexsize,color) ;
     %from the desired linear velocity computing the joint angle rate
     Jac = kuka_robot.jacob0(Q);
     %using the psudo inverse is not good idea here because the jitter
@@ -50,7 +54,7 @@ for j =1:1:sample_num
 %     kuka_robot.plot(Q,'workspace',[-3, 3 -3, 3 -3, 3]);
     
     %algorithm to estimate the normal direction
-    tool_lv_dot_global = 1000*tool_lv_dot_global;
+    tool_lv_dot_global = 200*tool_lv_dot_global;
     P_bar = eye(3)-n_hat*n_hat';
     n_hat_dot = -1*gamma_n*P_bar*L_n*n_hat;
     L_n_dot = -beta_n*L_n+(1/(1+norm(tool_lv_dot_global)^2))*tool_lv_dot_global*tool_lv_dot_global';
@@ -59,7 +63,7 @@ for j =1:1:sample_num
     L_n = L_n + L_n_dot;
     %visualization of the normal direction approaching from the initial
     %guess to the real direction
-    if(mod(sample_num,10) == 0)
+    if(mod(j,10) == 0)
         set_index = set_index + 1;
         n_hat_set(:,set_index) = n_hat;
         n_hat_distance(set_index) = dot(n_hat,T_tool_end_eff_cur(1:3,3)');
@@ -77,21 +81,6 @@ for j =1:1:sample_num
         pts = [nv_start; nv_end];
         plot3(pts(:,1), pts(:,2), pts(:,3),'m-');
         hold on;  
-        %draw tool: bar+square
-        myrmexsize = 0.08;
-        color = [0.3,0.6,0.8];
-        drawtactool(T_robot_end_eff_cur,T_tool_end_eff_cur,myrmexsize,color) ;
-        %draw contact ball
-        cx = -0.04 + 0.08*rand; %initialized contact point in the local x
-        cy = -0.04 + 0.08*rand; %initialized contact point in the local y
-        c = T_tool_end_eff_cur*[cx,cy,0,1]';
-        sphere_r = 0.002;
-        drawsphere(c(1:3),sphere_r);
-        % We consider about tool as a line segment and draw tool with line between two 3d points
-        P1 = T_robot_end_eff_cur(1:3,4)';
-        P2 = T_tool_end_eff_cur(1:3,4)';
-        pts = [P1; P2];
-        plot3(pts(:,1), pts(:,2), pts(:,3),'LineWidth',5);
     end
     %update the robot joint angle
     Q= Q+q_dot';
--- learntoolusage.m
+++ learntoolusage.m
@@ -24,6 +24,7 @@ tool_transform = tool_rotate * tool_translate ;
 %the tactool end-effector frame after the transformation(translationa nd rotation
 % from the robot end-effector frame)at the initialized status
 T_tool_end_eff_init = T_robot_end_eff_init*tool_transform;
+T_tool_end_eff_init(1:3,3)
 % kuka_robot.plot(Q,'workspace',[-3, 3 -3, 3 -3, 3]);
 hold on;
 trplot(T_tool_end_eff_init, 'frame', 'R','length',0.02,'width',0.01);
@@ -40,11 +41,18 @@ virtual_z = 26;
 rot_tm = rpy2tr(virtual_x,virtual_y,virtual_z,'deg');
 T_tool_end_eff_init_noise = T_tool_end_eff_init*rot_tm;
 % trplot(T_tool_end_eff_init_noise, 'frame', 'N');
+T_tool_end_eff_init_noise(1:3,3)
 
+%draw contact ball
+cx = -0.04 + 0.08*rand; %initialized contact point in the local x
+cy = -0.04 + 0.08*rand; %initialized contact point in the local y
+c = T_tool_end_eff_init*[cx,cy,0,1]';
+sphere_r = 0.002;
+drawsphere(c(1:3),sphere_r);
 
 %estimate normal direction using the initialized nv guess from the
 %approaching trajectory
-n_hat = est_nv_tac(kuka_robot,Q,tool_transform,T_tool_end_eff_init_noise);
+n_hat = est_nv_tac(kuka_robot,Q,tool_transform,T_tool_end_eff_init_noise)
 
 % estimate rotate angle from the virtual tool frame to real tool frame
 rotate_angle = est_rotate(virtual_angle);
